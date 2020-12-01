595b4de7edcd3bbfb2cc75d9eb12f2755096e388
Nguyennhuy <khanhandlinh12345@gmail.com>
2020-12-01 13:18:49 +0700

    Create project learn js for F8

---
+++ promise.js
@@ -0,0 +1,18 @@
+// Promise 
+// 		Sync : dong' bo,
+// 		ASync: Bat dong' bo.,
+// 		Noi dau(Pain)
+// 		Lys thuyet cach hoat dong
+
+
+var promise = new Promise(
+
+	function (resolve,reject) {
+		//Logic xu? ly
+		//resolve: THanhcong	 
+	}
+);  
+
+
+promise
+	
